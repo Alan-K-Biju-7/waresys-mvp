af6ab157696800e01fd73fdb45cc5a84c20ac51f
Alan-K-Biju-7 <alan.k.biju25@gmail.com>
2025-11-02 21:07:47 +0530

    chore(tasks): configure logger for Celery tasks

--- app/tasks.py
+++ app/tasks.py
@@ -10,5 +10,7 @@ from app import models, crud
 
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", os.getenv("REDIS_URL", "redis://redis:6379/0"))
 CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", os.getenv("REDIS_URL", "redis://redis:6379/0"))
-
 celery_app = Celery("waresys", broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
