3cb58d0795a1141b64ed1e540dc87b9c7b39a588
Alan-K-Biju-7 <alan.k.biju25@gmail.com>
2025-11-03 18:48:01 +0530

    feat(schemas): define ProductIn and ProductOut models

--- app/schemas.py
+++ app/schemas.py
@@ -3,3 +3,15 @@ from datetime import datetime, date
 from typing import Optional, List, Any
 from pydantic import BaseModel, Field, StringConstraints
 from typing_extensions import Annotated
+
+# ------ Products ------
+class ProductIn(BaseModel):
+    sku: str
+    name: str
+    category_id: Optional[int] = None
+    uom: str = "pcs"
+    reorder_point: float = 0
+
+class ProductOut(ProductIn):
+    id: int
+    model_config = {"from_attributes": True}
